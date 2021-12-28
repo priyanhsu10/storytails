bd4cd51f3ffc191dcf6253df5069159b13020ca6
priyanshu10 <priyanshuparate@gmail.com>
2021-12-28 10:50:30 +0530

    change placeholder

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -21,7 +21,7 @@ export default function Search({apply,clear}) {
         <div>
             
 <div className="container input-group mb-3 ">
-  <input type="text" className="form-control"  ref={searchref} placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+  <input type="text" className="form-control"  ref={searchref} placeholder="Enter story to serach" aria-label="Recipient's username" aria-describedby="button-addon2"/>
   <button className="btn btn-outline-primary" type="button" id="button-addon2" onClick={()=>onaApply()}>Search</button>
   <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={()=>onClear()}>Clear</button>
 </div>
